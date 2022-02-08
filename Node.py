f77531b961bb6bf2126d19590621856061488011
wobn1ar <williamorringe@gmail.com>
2022-02-08 16:25:03 +0000

    Init

--- Node.py
+++ Node.py
@@ -1,4 +1,10 @@
 class Node:
-    def __init__(self, index: int, neighbours: [[]]):
+    def __init__(self, index: int):
         self.index = index
-        self.neighbours = neighbours  # [[Node, Weight], [Node2, Weight2], ...]
+        self.neighbours = []
+
+    def give_neighbours(self,  neighbours: []):
+        self.neighbours.append(neighbours)  # [[Node, Weight], [Node2, Weight2], ...]
+
+    def __str__(self):
+        return str(self.neighbours)
--- main.py
+++ main.py
@@ -1,8 +1,27 @@
+from Node import Node
+
+
 def q_learning():
     q_tables = [[]]
     for i in range(10):
         print("hi")
 
 
+def initialise_nodes():
+    nodes = []
+    for index in range(10):
+        nodes.append(Node(index))
+    for index in range(9):
+        nodes[index].give_neighbours([nodes[index+1], 1])
+    nodes[9].give_neighbours([nodes[0], 1])
+    return nodes
+
+
+def print_graph(nodes):
+    for node in nodes:
+        print(str(node))
+
+
 if __name__ == "__main__":
-    print(":(")
+    graph = initialise_nodes()
+    print_graph(graph)
